699fb8bc5d351c016e861098998c8e033c15c38e
Augusto Brito <britoaugusto1@gmail.com>
2024-09-28 10:42:22 -0300

    agregamos un logo al navbar del proyecto

--- src/commons/Menu.jsx
+++ src/commons/Menu.jsx
@@ -1,13 +1,19 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { Link } from "react-router-dom";
-
+import logo from "../../src/assets/logo-cocina.jpg"
 function Menu() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand >Crud Cocina</Navbar.Brand>
+        <Navbar.Brand>
+          <img
+            src={logo}
+            alt="logo Rolling Coffee"
+            className="img-fluid"
+            width={85}
+          />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
